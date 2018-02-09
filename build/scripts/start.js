3abac290fc52654a0d2b6cfdced3f6beacf9e5b1
joaker <jooakley@gmail.com>
2018-02-09 16:57:30 -0600

    bind the heroku port or 3000

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,7 @@
 const logger = require('../lib/logger');
+import port from '../port';
 
 logger.info('Starting server...');
-require('../../server/main').listen(3000, () => {
+require('../../server/main').listen(port, () => {
     logger.success('Server is running at http://localhost:3000');
 });
